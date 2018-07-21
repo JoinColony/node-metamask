58bcd0bec0a492c421789512c50e29ff47e27f97
Philip Cavaco <philipcavaco@gmail.com>
2018-07-21 02:20:24 +0100

    Fix method resolution and associate responses with correct callback.

--- RemoteMetaMaskProvider.js
+++ RemoteMetaMaskProvider.js
@@ -1,6 +1,7 @@
 class RemoteMetaMaskProvider {
   constructor(connector) {
     this._connector = connector;
+    this._callbacks = {};
   }
   _getAsyncMethod(method) {
     // Sync methods don't work with MetaMask
@@ -17,31 +18,60 @@ class RemoteMetaMaskProvider {
       'eth_hashrate',
       'eth_gasPrice',
       'eth_accounts',
-      'eth_blocknumber',
+      'eth_blockNumber',
     ];
     const idx = syncMethods.indexOf(method);
     if (idx >= 0) {
       return syncMethods[idx].replace(/(.+)\_([a-z])(.+)/, (str, p1, p2, p3) => `${p1}_get${p2.toUpperCase()}${p3}`)
     }
-    if (method === 'net_version') {
-      // Special MetMask fix
-      method = 'version_getNetwork';
+    const translateMethod = {
+      net_version: 'version_getNetwork',
+      eth_getLogs: 'eth_filter',
+      eth_getTransactionByHash: 'eth_getTransaction'
+    }
+    if (method in translateMethod) {
+      method = translateMethod[method];
     }
     return method;
+  }
+  _guid() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
   }
   send(payload, callback) {
     if (!this._connector.ready()) {
       return callback(new Error('Can\'t send. Not connected to a MetaMask socket.'))
     }
+    // Because requests are handled across a WebSocket they need to be
+    // associated with their callback with an ID which is returned with the 
+    // response.
+    const request_id = this._guid();
+    this._callbacks[request_id] = callback;
     payload.method = this._getAsyncMethod(payload.method);
-    this._connector.send('execute', payload, 'executed').then(result => {
-      if (result && result.error) return callback(new Error(result.error));
-      callback(null, {
+    this._connector.send('execute', request_id, payload, 'executed').then(({
+      request_id: responseRequestId,
+      result
+    }) => {
+      const request_callback = this._callbacks[responseRequestId];
+      if (request_callback === undefined) {
+        return; // A response for this request was already handled
+      } else {
+        delete this._callbacks[responseRequestId];
+      }
+      if (result && result.error) return request_callback(new Error(result.error));
+      request_callback(null, {
         id: payload.id,
         jsonrpc: '2.0',
         result,
       });
     }).catch(err => callback(err));
+  }
+  sendAsync(payload, callback) {
+    this.send(payload, callback)
   }
 }
 
--- client/index.js
+++ client/index.js
@@ -6,7 +6,7 @@
     return addLog('Please unlock MetaMask first and then reload this page');
   }
   const socket = new WebSocket('ws://localhost:3333');
-  const reply = (action, payload) => socket.send(JSON.stringify({ action, payload }));
+  const reply = (action, request_id, payload) => socket.send(JSON.stringify({ action, request_id, payload }));
   socket.onmessage = msg => {
     let message;
     try {
@@ -15,24 +15,24 @@
       return addLog('Could not parse websocket message. Is it a proper JSON command?');
     }
     if (message.action === 'execute') {
-      executeAction(message.payload, reply);
+      executeAction(message.request_id, message.payload, reply);
     }
   }
 
-  async function executeAction({ method, params}, reply) {
+  async function executeAction(request_id, { method, params}, reply) {
     let result;
-    addLog(`Calling ${method} with ${JSON.stringify(params)}`);
+    addLog(`Request ${request_id} Calling ${method} with ${JSON.stringify(params)}`);
     try {
-      result = await execute(method, params);
+      result = await execute(request_id, method, params);
     } catch(e) {
-      return reply('executed', {
+      return reply('executed', request_id, {
         error: e.message
       });
     }
-    reply('executed', result);
+    reply('executed', request_id, result);
   }
 
-  function execute(method, params) {
+  function execute(request_id, method, params) {
     return new Promise((resolve, reject) => {
       const splitMethod = method.split('_');
       const scope = splitMethod[0];
@@ -41,6 +41,7 @@
         if (err) {
           return reject(err);
         }
+        addLog(`Result from ${request_id} ${method}: ${JSON.stringify(result)}`);
         resolve(result);
       })
       try {
